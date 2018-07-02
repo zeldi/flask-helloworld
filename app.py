08240e3ede6298b00b6b40652dfae122e66bcdd2
Zeldi Suryady <zeldi.suryady@gmail.com>
2018-07-02 18:45:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def hello():
     return "Hello from Python!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
